9f455323703eb38c954d7d60c0bf46a4a4d6dac8
Franced3000 <franc.spata@gmail.com>
2024-06-29 14:44:22 +0200

    aggiunto middleware e validator

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,6 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  // Implementazione dell'autenticazione...
-  next();
+  const token = req.headers.authorization?.split(' ')[1];
+  
+  if (!token) {
+    return res.status(401).json({ message: 'Accesso negato. Nessun token fornito.' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+    (req as any).user = decoded;
+    next();
+  } catch (err) {
+    res.status(400).json({ message: 'Token non valido.' });
+  }
 };
+
--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,7 +1,68 @@
-import { body } from 'express-validator';
+import { body, param } from 'express-validator';
 
 export const eventValidator = [
-  body('titolo').notEmpty().withMessage('Il titolo è obbligatorio'),
-  body('data').isDate().withMessage('La data non è valida'),
-  // Altri validatori...
+  body('titolo')
+    .notEmpty()
+    .withMessage('Il titolo è obbligatorio')
+    .isString()
+    .withMessage('Il titolo deve essere una stringa'),
+  body('descrizione')
+    .optional()
+    .isString()
+    .withMessage('La descrizione deve essere una stringa'),
+  body('data')
+    .notEmpty()
+    .withMessage('La data è obbligatoria')
+    .isISO8601()
+    .withMessage('La data deve essere in formato ISO 8601'),
+  body('luogo')
+    .notEmpty()
+    .withMessage('Il luogo è obbligatorio')
+    .isString()
+    .withMessage('Il luogo deve essere una stringa'),
+  body('posti_disponibili')
+    .isInt({ min: 1 })
+    .withMessage('I posti disponibili devono essere almeno 1'),
+];
+
+export const userValidator = [
+  body('nome')
+    .notEmpty()
+    .withMessage('Il nome è obbligatorio')
+    .isString()
+    .withMessage('Il nome deve essere una stringa'),
+  body('cognome')
+    .notEmpty()
+    .withMessage('Il cognome è obbligatorio')
+    .isString()
+    .withMessage('Il cognome deve essere una stringa'),
+  body('email')
+    .notEmpty()
+    .withMessage('L\'email è obbligatoria')
+    .isEmail()
+    .withMessage('Formato email non valido'),
+  body('password')
+    .notEmpty()
+    .withMessage('La password è obbligatoria')
+    .isLength({ min: 6 })
+    .withMessage('La password deve essere lunga almeno 6 caratteri'),
+];
+
+export const registrationValidator = [
+  body('evento_id')
+    .notEmpty()
+    .withMessage('L\'ID dell\'evento è obbligatorio')
+    .isInt()
+    .withMessage('L\'ID dell\'evento deve essere un numero intero'),
+  body('utente_id')
+    .notEmpty()
+    .withMessage('L\'ID dell\'utente è obbligatorio')
+    .isInt()
+    .withMessage('L\'ID dell\'utente deve essere un numero intero'),
+];
+
+export const idParamValidator = [
+  param('id')
+    .isInt()
+    .withMessage('L\'ID deve essere un numero intero'),
 ];
